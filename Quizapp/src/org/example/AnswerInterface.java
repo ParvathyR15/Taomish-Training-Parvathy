faf4aab497a002251f8c350b51b7b33f44b57e4e
Parvathy <parvathyr152002@gmail.com>
2025-02-05 17:59:20 +0530

    commited 2 projects

---
+++ Quizapp/src/org/example/AnswerInterface.java
@@ -0,0 +1,7 @@
+
+package org.example;
+
+public interface AnswerInterface {
+    boolean checkAnswer(String question, String answer);
+}
+
---
+++ Quizapp/src/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name + " Let's begin the quiz");
+
+
+        Question question = new Question();
+        Answers answer = new Answers();
+        Score score = new Score();
+
+
+        for (String questionText : ((Question) question).quiz.keySet()) {
+            question.showQuestions(questionText);
+
+
+            System.out.println("\nEnter your answer (A, B, C, D):");
+            String userAnswer = scanner.nextLine().toUpperCase();
+
+
+            if (answer.checkAnswer(questionText, userAnswer)) {
+                System.out.println("Correct answer!");
+                score.increment();
+            } else {
+
+                String correctAnswer = answer.getCorrectAnswer(questionText);
+                System.out.println("Wrong answer. The correct answer is " + correctAnswer);
+            }
+        }
+
+
+        System.out.println("\nYour final score is: " + score.fetchScore());
+
+        scanner.close();
+    }
+}
+
